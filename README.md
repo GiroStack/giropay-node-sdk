5aad40ffed2a41fd92e5c39d413573660a9d3638
dapperwalze <dapperwalze@Walzes-MacBook-Pro.local>
2024-10-22 12:27:02 +0100

    chore: node-sdk setup

---
+++ README.md
@@ -0,0 +1,11 @@
+# GiroPay Node.js SDK
+
+GiroPay SDK is a comprehensive Node.js SDK designed to simplify the integration with GiroPay’s powerful API. It provides developers with an easy-to-use interface for accessing core GiroPay services, such as initiating transactions, retrieving account balances, and more.
+
+## Install
+
+```bash
+npm install @giropay/node-sdk
+# or
+yarn add @giropay/node-sdk
+```
---
+++ lib/types/index.d.ts
@@ -0,0 +1,12 @@
+export declare interface useSnapChatConnectionReturnType {
+    snapchatData: Record<string, any> | null;
+    isLoading: boolean;
+    onSnapChatConnect: () => void;
+};
+
+export declare interface FintechSDK {
+    getAccountBalance: (accountId: string) => Promise<number>;
+    transferMoney: (fromAccountId: string, toAccountId: string, amount: number) => Promise<boolean>;
+    getTransactionHistory: (accountId: string, startDate: Date, endDate: Date) => Promise<any[]>;
+    createAccount: (userId: string, accountType: 'checking' | 'savings') => Promise<string>;
+  }
